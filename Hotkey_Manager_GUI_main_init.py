bb430a0cdf8d889a60cbdd87a7d1dc4472b3f954
LizzardHD <107073194+LizzardHD@users.noreply.github.com>
2023-09-19 04:55:08 +0200

    Update Hotkey_Manager_GUI_main_init.py

--- Hotkey_Manager_GUI_main_init.py
+++ Hotkey_Manager_GUI_main_init.py
@@ -180,9 +180,13 @@ class Hotkey_Manager:
         # Upper half of left frame
         self.control_frame = tk.Frame(self.manager_frame)
         self.control_frame.grid(row=0, column=0, padx=10, pady=(0, 10))
+        # Create a new window for importing data in left frame
+        self.load_window = tk.Frame(self.manager_frame)
         # Lower half of left frame
-        self.action_canvas = tk.Canvas(self.manager_frame)
+        self.action_canvas = tk.Canvas(self.manager_frame,highlightthickness=0,bg="grey")
         self.action_canvas.grid(row=3, column=0)
+        # Create a new window for exporting data in left frame
+        self.save_window = tk.Frame(self.manager_frame)
         # Upper Half of right frame
         self.execution_frame = tk.Frame(self.terminal_frame)
         self.execution_frame.grid(row=0, column=0)
@@ -194,21 +198,22 @@ class Hotkey_Manager:
         self.action_frame = tk.Frame(self.action_canvas)
         self.action_frame_place = self.action_canvas.create_window((0, 0), window=self.action_frame, anchor="n")
         # Buttons for data functionality in upper half of left frame
-        add_action_button = tk.Button(self.control_frame, text="Add Action", command=self.add_action)
-        add_action_button.grid(row=1, column=0, columnspan=2)
-        save_button = tk.Button(self.control_frame, text="Save Actions", command=self.save)
+        add_action_button = ttk.Button(self.manager_frame, text="Add Action", command=self.add_action,style="Custom.TButton")
+        add_action_button.grid(row=4, columnspan=10, sticky="s")
+        save_button = ttk.Button(self.control_frame, text="Save Actions", command=self.save,style="Custom.TButton")
         save_button.grid(row=0, column=1)
-        Load_button = tk.Button(self.control_frame, text="Load Actions", command=self.load)
+        Load_button = ttk.Button(self.control_frame, text="Load Actions", command=self.load,style="Custom.TButton")
         Load_button.grid(row=0, column=0)
         # Terminal space to display feedback in lower half of right frame
-        terminal_label = tk.Label(self.terminal_frame,text="Terminal:")
+        terminal_label = ttk.Label(self.terminal_frame,text="Terminal:")
         terminal_label.grid(row=1, column=0, sticky="w")
         self.terminal = scrolledtext.ScrolledText(self.terminal_frame, wrap=tk.WORD, state=tk.DISABLED, width=75, height=10)
+        self.terminal.configure(font=("Helvetica", 9))
         self.terminal.grid(row=2, column=0, sticky="n")
         # Buttons for hotkey functionality in upper Half of right frame
-        self.start_button = tk.Button(self.execution_frame, text="Start",command=Hotkey_main_instance.start_programm)
+        self.start_button = ttk.Button(self.execution_frame, text="Start",command=Hotkey_main_instance.start_programm,style="Custom.TButton")
         self.start_button.grid(row=0, column=0, padx=5)
-        self.stop_button = tk.Button(self.execution_frame, text="Stop",command=Hotkey_main_instance.stop_programm)
+        self.stop_button = ttk.Button(self.execution_frame, text="Stop",command=Hotkey_main_instance.stop_programm,style="Custom.TButton")
         # Add the name/key of Import Optionmenu
         self.import_name_var = tk.StringVar()
         self.import_name_var.set("") # Default selection
@@ -217,10 +222,19 @@ class Hotkey_Manager:
         self.name_key = tk.StringVar()
         self.name_key.set(" ") # Default Selection
         execution_options = list(self.data.keys()) if self.data else [""]
-        self.name_option = tk.OptionMenu(self.execution_frame, self.name_key, *execution_options, command=self.update_name_option)
-        self.name_option.grid(row=1, column=0, pady=10)
+        execution_optionmenu_border = tk.Frame(self.execution_frame, borderwidth=1,relief="groove")
+        execution_optionmenu_border.grid(row=1, column=0, pady=10)
+        self.name_option = ttk.OptionMenu(execution_optionmenu_border, self.name_key, *execution_options, command=self.update_name_option)
+        self.name_option.grid()
         self.name_option.config(width=25)
         self.update_name_option()  # Initialize the OptionMenu
+        # Create a Sytle
+        self.style = ttk.Style()
+        # Define a custom style for the normal state
+        self.style.configure("Custom.TButton", relief="solid")
+        # Initialize Hotkey
+        self.add_action("Hotkey")
+    
     # Functions for the Execution Optionmenu
     def update_name_option(self):
         # Clear the existing options
@@ -236,7 +250,7 @@ class Hotkey_Manager:
         if self.display_message_run_times > 999:
             self.display_message_run_times = 0
         self.terminal.config(state=tk.NORMAL)  # Set the state to normal to allow editing
-        self.terminal.insert(tk.END, f"------------------------------------{self.display_message_run_times:03}------------------------------------")
+        self.terminal.insert(tk.END, f"---------------------------------------------------------------{self.display_message_run_times:03}---------------------------------------------------------------")
         self.terminal.insert(tk.END, "\n")
         self.terminal.insert(tk.END, message)  # Insert the message
         self.terminal.insert(tk.END, "\n")
@@ -258,36 +272,38 @@ class Hotkey_Manager:
             return {}
     # Function to update the canvas and action frame position
     def canvas_change(self,event=None):
-        # Configure the action_canvas scroll region to fit all widgets
-        self.action_canvas.configure(scrollregion=self.action_canvas.bbox("all"))
-        # Check if the canvas width is greater than the action_frame's requested width
-        if self.action_canvas.winfo_width() > self.action_frame.winfo_reqwidth():
-            # Calculate the x-position to center the action_frame
-            x_position = (self.action_canvas.winfo_width() - self.action_frame.winfo_reqwidth()) / 2 + self.action_frame.winfo_reqwidth() / 2
-            # Set the position of the action_frame within the canvas
-            self.action_canvas.coords(self.action_frame_place, x_position, 0)
-        else:
-            # Center the action_frame in the canvas when canvas is smaller
-            x_position = self.action_canvas.winfo_width() / 2
-            self.action_canvas.coords(self.action_frame_place, x_position, 0)
+        # Define the width of the canvas
+        self.action_canvas.config(width=self.action_frame.winfo_reqwidth())
+        # Calculate the x-position to center the action_frame
+        x_position = (self.action_canvas.winfo_width() - self.action_frame.winfo_reqwidth()) / 2 + self.action_frame.winfo_reqwidth() / 2
+        # Set the position of the action_frame within the canvas
+        self.action_canvas.coords(self.action_frame_place, x_position, 0)
         # Calculate the total height required for all child widgets in the action_frame
         total_child_height = sum(child.winfo_reqheight() for child in self.action_frame.winfo_children())
+        # Configure the action_canvas scroll region to fit all widgets
+        self.action_canvas.configure(scrollregion=(0,0,0,total_child_height))
         # Set a maximum canvas height to avoid excessive scrolling
-        self.action_canvas.config(height=min(300, total_child_height))
+        self.action_canvas.config(height=min(150, total_child_height))
+        # Show the scrollbar and adjust the canvas scroll region
+        self.scrollbar.grid(row=2, column=1, sticky="ns",padx=(10,0))
+        self.action_canvas.config(scrollregion=(0,0,0,total_child_height))
+        '''
         # Check if the action_frame height exceeds the canvas height
         if self.action_frame.winfo_reqheight() > self.action_canvas.winfo_height():
-            # Show the scrollbar and adjust the canvas scroll region
-            self.scrollbar.grid(row=2, column=1, sticky="ns")
-            self.action_canvas.config(scrollregion=self.action_canvas.bbox("all"))
         else:
             # Hide the scrollbar and adjust the canvas scroll region
             self.scrollbar.grid_remove()
-            self.action_canvas.config(scrollregion=self.action_canvas.bbox("all"))
+            self.action_canvas.config(scrollregion=(0,0,0,total_child_height))
+        '''
+        print(self.action_add_run_times," - runtime")
+        print(self.action_canvas.winfo_height()," - canvas")
+        print(self.action_frame.winfo_reqheight()," - frame")
+        print(total_child_height," - childs")
         self.update_name_option()
-        # Update the canvas to reflect changes
-        self.action_canvas.update_idletasks()
+        
     # Function to add an action
     def add_action(self,action_type="Sleep", action_value=""):
+        self.action_canvas.event_generate("<Configure>")
         if self.action_add_run_times == 0:
             action_type = "Hotkey"
         if action_type == "Hotkey":
@@ -298,18 +314,20 @@ class Hotkey_Manager:
         self.action_add_run_times += 1
 
         new_action_frame = tk.Frame(self.action_frame)
-        new_action_frame.grid(row=len(self.action_type_comboboxes) + 1, column=0, sticky="ew")
+        new_action_frame.grid(row=len(self.action_type_comboboxes) + 1, column=0,padx=10)
         self.action_new_frame_list.append(new_action_frame)
-
-        new_action_label = tk.Label(new_action_frame, text=f"{self.action_add_run_times:03}")
+        
+        new_action_label = ttk.Label(new_action_frame, text=f"{self.action_add_run_times:03}",padding=(1,2))
         new_action_label.grid(row=0, column=0)
 
         new_action_type_combobox = tk.StringVar()
         new_action_type_combobox.set(action_type)  # Default Selection
+        action_type_optionmenu_border = tk.Frame(new_action_frame, borderwidth=1,relief="groove")
+        action_type_optionmenu_border.grid(row=0, column=1)
         if action_type == "Hotkey":
-            new_action_type_option_menu = tk.OptionMenu(new_action_frame, new_action_type_combobox, "Hotkey")
+            new_action_type_option_menu = ttk.OptionMenu(action_type_optionmenu_border, new_action_type_combobox, "Hotkey")
         else:
-            new_action_type_option_menu = tk.OptionMenu(new_action_frame, new_action_type_combobox,
+            new_action_type_option_menu = ttk.OptionMenu(action_type_optionmenu_border, new_action_type_combobox,
                                                         "Sleep",                "Mouse Click",
                                                         "Mouse Double Click",   "Mouse Press",
                                                         "Mouse Release",        "Absolute Mouse Move", 
@@ -317,24 +335,25 @@ class Hotkey_Manager:
                                                         "Keyboard Send",        "Keyboard Write",
                                                         "Keyboard Press",       "Keyboard Release")
         new_action_type_option_menu.config(width=20)
-        new_action_type_option_menu.grid(row=0, column=1, sticky="w")
+        new_action_type_option_menu.grid(row=0, column=0)
 
-        new_action_value_entry = tk.Entry(new_action_frame, width=25)
-        new_action_value_entry.grid(row=0, column=2, sticky="e")
+        new_action_value_entry = ttk.Entry(new_action_frame, width=25,takefocus=False)
+        new_action_value_entry.grid(row=0, column=2, pady=(1,3))
         new_action_value_entry.insert(0, action_value)  # Default Value
 
-        new_delete_action_button = tk.Button(self.manager_frame, text="X", command=self.delete_action)
-        new_delete_action_button.grid(row=4, columnspan=10, sticky="s")
+        new_delete_action_button = ttk.Button(self.manager_frame, text="X",style="Custom.TButton", command=self.delete_action)
+        new_delete_action_button.grid(row=5, columnspan=10, sticky="s")
 
         new_action_frame.grid_columnconfigure(0, weight=1)
         new_action_frame.grid_columnconfigure(1, weight=1)
 
         self.action_type_comboboxes.append(new_action_type_combobox)
         self.action_value_entries.append(new_action_value_entry)
-
-        self.canvas_change(None)  # Update the canvas and action frame position
+        
+        self.action_canvas.event_generate("<Configure>")
         self.action_canvas.yview_moveto(1.0)
     def delete_action(self):
+        self.action_canvas.event_generate("<Configure>")
         # Check if there are actions to delete
         if self.action_add_run_times > 1:
             # Remove the last new action frame from the GUI
@@ -347,26 +366,28 @@ class Hotkey_Manager:
             del self.action_new_frame_list[-1]
         else:
             self.display_message("cannot delete Hotkey")
-        # Reconfigure the canvas and update its position
-        self.canvas_change(None)
+        self.action_canvas.event_generate("<Configure>")
+        self.action_canvas.yview_moveto(1.0)
     # Functions for loading and deleting data
     def load(self):
+        # Clear Space to prevent stacking
+        self.load_window.destroy()
         # Reload data
         self.data.update(self.Load_exported_data())
         # Create a new window for importing data
-        self.Load_window = tk.Frame(self.manager_frame)
-        self.Load_window.grid(row=2, padx=10, pady=(0, 10))
+        self.load_window = tk.Frame(self.manager_frame)
+        self.load_window.grid(row=2, padx=10, pady=(0, 10))
         import_options = list(self.data.keys()) if self.data else [""]
-        Load_option_menu = tk.OptionMenu(self.Load_window, self.import_name_var, *import_options)
+        Load_option_menu = ttk.OptionMenu(self.load_window, self.import_name_var, *import_options)
         Load_option_menu.config(width=25)
         # Add a "Load" button
-        Load_button = tk.Button(self.Load_window, text="Load", command=self.load_action_set)
+        Load_button = ttk.Button(self.load_window, text="Load", command=self.load_action_set,style="Custom.TButton")
         # Add a "Delete" button
-        delete_button = tk.Button(self.Load_window, text="Delete", command=self.delete_action_set)
+        delete_button = ttk.Button(self.load_window, text="Delete", command=self.delete_action_set,style="Custom.TButton")
         # Give No Data Feedback
-        Load_empty_list_fault = tk.Label(self.Load_window, text="No Data")
+        Load_empty_list_fault = ttk.Label(self.load_window, text="No Data")
         # Add a "Close" button
-        close_button = tk.Button(self.Load_window, text="Close", command=self.Load_window.destroy)
+        close_button = ttk.Button(self.load_window, text="Close", command=self.load_window.destroy,style="Custom.TButton")
         close_button.grid(row=3, columnspan=2)
         if import_options != []:
             Load_option_menu.grid(row=1, columnspan=2)
@@ -395,7 +416,7 @@ class Hotkey_Manager:
         else:
             self.display_message(f"No data found for name: {self.selected_name}")
         # Close the Load window
-        self.Load_window.destroy()
+        self.load_window.destroy()
     def delete_action_set(self):
         self.selected_name = self.import_name_var.get()
         if self.selected_name in self.data:
@@ -405,15 +426,17 @@ class Hotkey_Manager:
             # Clear the name selection
             self.import_name_var.set("")
             # Close and reopen the window
-            self.Load_window.destroy()
+            self.load_window.destroy()
             self.load()
             # Give Feedback
-            Data_deleted = tk.Label(self.Load_window, text=f"{self.selected_name} deleted")
+            Data_deleted = ttk.Label(self.load_window, text=f"{self.selected_name} deleted")
             Data_deleted.grid(row=0, columnspan=2)
         else:
             self.display_message(f"No action set found for name: {self.selected_name}")
     # Functions for saving current Data
     def save(self):
+        # Clear Space to prevent stacking
+        self.save_window.destroy()
         # Reload data
         self.data.update(self.Load_exported_data())
         self.current_data = []
@@ -421,14 +444,14 @@ class Hotkey_Manager:
         self.save_window = tk.Frame(self.manager_frame)
         self.save_window.grid(row=5, padx=10, pady=10)
         # Add a Label and an Entry widget to input the name
-        name_label = tk.Label(self.save_window, text="Enter a name for this action set:")
+        name_label = ttk.Label(self.save_window, text="Enter a name for this action set:")
         name_label.grid()
-        self.name_entry = tk.Entry(self.save_window)
+        self.name_entry = ttk.Entry(self.save_window)
         self.name_entry.grid(pady=(0, 5))
         if self.imported_data:
             self.name_entry.insert(0, self.selected_name)
         # Add an "Enter" button to confirm the name
-        enter_button = tk.Button(self.save_window, text="Enter", command=self.save_action_set)
+        enter_button = ttk.Button(self.save_window, text="Enter", command=self.save_action_set,style="Custom.TButton")
         enter_button.grid()
     def save_action_set(self):
         self.data.update(self.Load_exported_data())
